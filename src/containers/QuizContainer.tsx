daf2d4a067660865b89f5ca9b5e96da5a509b2db
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-06-08 20:07:37 +0900

    add intro

--- src/containers/QuizContainer.tsx
+++ src/containers/QuizContainer.tsx
@@ -1,40 +1,67 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 import { PinkButton } from "../components/PinkButton";
 import { Quiz } from "../components/Quiz";
 import { RootState } from "../modules";
-import { next } from "../modules/result";
+import { next, reset } from "../modules/result";
 
 function QuizContainer() {
     // 상태 조회
   const page = useSelector((state : RootState) => state.result.page); // 여기서 result: modules 파일이름
   const dispatch = useDispatch();
-  const quiz = useSelector((state : RootState) => state.result.quizzes);
+  const quizzes = useSelector((state : RootState) => state.result.quizzes);
   const score = useSelector((state : RootState) => state.result.score);
   
+  const goHome = () => {
+    dispatch(reset())
+  }
   return (
     <>
       {page===0 && (
-        <main>
-          
-          <h1>자바스크립트 퀴즈</h1>
+        <Main>
+          <h1 style={{fontSize:'3rem'}}>😎 자바스크립트 퀴즈 💻</h1>
+          <Introduction cnt={quizzes.length} />
           <PinkButton text="퀴즈 시작!" clickEvent={() => {
             dispatch(next())
           }} />
-        </main>
+        </Main>
       )}
-      {page > 0 && page <= quiz.length && (
-        <Quiz></Quiz>
+      {page > 0 && page <= quizzes.length && (
+        <Main>
+          <button onClick={goHome}>홈으로</button>
+          <Quiz/>
+        </Main>
       )}
-      {page > quiz.length && (
-        <main>
+      {page > quizzes.length && (
+        <Main>
           우와 끝! 당신의 점수는?
 
           {score}
-        </main>
+        </Main>
       )}
     </>
   );
 }
+
+const Introduction = ({cnt}: {
+  cnt: number
+}) => {
+  return (
+    <>
+      <p style={{fontSize:'1.3rem'}}>
+        당신의 자바스크립트 지식을 확인해보세요! <br/>
+        면접을 준비하고 있다면, 자주 들어와서 반복해서 풀어보는 것도 좋은 방법이에요.<br/>
+      </p>
+      <h2>
+        총 {cnt}문제
+      </h2>
+    </>
+  )
+}
+
+const Main = styled.main`
+text-align: center;
+`
 
 export default QuizContainer;
