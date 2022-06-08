5d011e8d785484ee751afff640420b1aca008232
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-06-08 22:36:31 +0900

    레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import QuizContainer from "./containers/QuizContainer";
 import "./App.scss"
+import Layout from "./layout/Layout";
 
 const App: React.FC =()=> {
 
   return (
-    <>
+    <Layout>
       <QuizContainer />
-    </>
+    </Layout>
   );
 }
 
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+import styled from "styled-components"
+
+const Layout = (props: {
+    children: React.ReactNode
+}) => {
+    return (
+        <Container>
+            <Main>
+                {props.children}
+            </Main>
+            <Footer>
+                <a href='https://github.com/pullingoff'>
+                2022 @pullingoff
+                </a>
+            </Footer>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+display: flex;
+flex-flow: column;
+align-items: center;
+text-align: center;
+`
+
+const Main = styled.main`
+margin-top: 20px;
+`
+
+const Footer = styled.footer`
+display: block;
+padding: 1rem;
+position: fixed;
+bottom: 0;
+font-weight: bold;
+& a:hover{
+    color: salmon;
+}
+`
+
+export default Layout;
