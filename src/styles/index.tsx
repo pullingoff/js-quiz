e26125c9c8a87f7f19b8577bd2648313fa776068
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-06-09 13:44:21 +0900

    style change

---
+++ src/styles/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Header = styled.h1`
+font-size: 3rem;
+`
+
+export const ResetBtn = styled.button`
+border: 1px solid lightgrey;
+margin: auto;
+font-size: 1.3rem;
+background: transparent;
+font-weight: bold;
+cursor: pointer;
+&:hover {
+  background: lightgrey;
+}
+`
